3c42572fde90009c3cf705a942f8ba71e8df64c5
lily64mark <lily64mark@outlook.com>
2026-08-01 13:57:27 +0530

    Create ExpenseList.js

---
+++ src/components/ExpenseList.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./ExpenseList.css";
+
+function ExpenseList(){
+
+return(
+<div>
+
+<h2>Expense History</h2>
+
+<p>No expenses added yet.</p>
+
+</div>
+)
+
+}
+
+export default ExpenseList;
