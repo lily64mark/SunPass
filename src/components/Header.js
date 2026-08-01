04e010b0e5bcc2e510f769bce7ede8b854510668
lily64mark <lily64mark@outlook.com>
2026-08-01 13:57:02 +0530

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Header.css";
+
+function Header(){
+
+return(
+<header>
+<h1>SunPass Toll Manager</h1>
+<p>Manage your toll expenses easily</p>
+</header>
+)
+
+}
+
+export default Header;
