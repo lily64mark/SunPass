3983eae4127881c254d306342b9abacfb03c76bc
lily64mark <lily64mark@outlook.com>
2026-08-01 13:57:55 +0530

    Create Receipt.js

---
+++ src/components/Receipt.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Receipt.css";
+
+function Receipt(){
+
+return(
+<div>
+
+<h2>Receipt Details</h2>
+
+<p>Your toll receipts will appear here.</p>
+
+</div>
+)
+
+}
+
+export default Receipt;
